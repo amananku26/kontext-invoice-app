49898dea145feec05efe3cd832adddcdd198ac45
AMAN ANKU <amankumaranku@gmail.com>
2020-12-02 13:40:04 +0530

    Updates

---
+++ precache-manifest.6eb69928b5cbdcf822066fad347a4c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d12c71494099a984d7029deb57a2d7",
+    "url": "/kontext-invoice-app/index.html"
+  },
+  {
+    "revision": "038e141eaea905d1b8ce",
+    "url": "/kontext-invoice-app/static/css/main.5d001cc0.chunk.css"
+  },
+  {
+    "revision": "c2f1b785b405aa24e411",
+    "url": "/kontext-invoice-app/static/js/2.bef25fff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kontext-invoice-app/static/js/2.bef25fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038e141eaea905d1b8ce",
+    "url": "/kontext-invoice-app/static/js/main.afc66a2c.chunk.js"
+  },
+  {
+    "revision": "ee91ffdd3126c2f04de4",
+    "url": "/kontext-invoice-app/static/js/runtime-main.1e978b79.js"
+  }
+]);
